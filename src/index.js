7cca7c2b52bbee6aaf300d02999ad73c8056e2f3
sndsabin <contact@sabinb.com.np>
2017-08-25 01:36:36 +0545

    Second Commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Main from './Main';
-
-let index = new Main();
-
-
-
--- src/miscellaneous.js
+++
@@ -1,9 +0,0 @@
-export function generateRandomNumber(min, max) 
-{
-	return Math.ceil(Math.random() * (max-min) + min);
-}
-
-export function shuffle(array)
-{
-	array.sort(() => (Math.random() - 0.5));
-}
